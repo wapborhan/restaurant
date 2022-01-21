7ae9422dd516b99735e4f326cd7ae5ea62e296b4
Borhan Uddin <borhaninfos@gmail.com>
2022-01-21 13:58:34 +0600

    Create dishes.js

---
+++ src/data/dishes.js
@@ -0,0 +1,20 @@
+const DISHES = [
+  {
+    id: 0,
+    name: "Ginger Burger",
+    image: "",
+    category: "Meal",
+    label: "Hot",
+    price: "999",
+    description: "Unique Dishes",
+    comments: [
+      {
+        id: 0,
+        rating: 5,
+        comment: "Good Test",
+        author: "Borhan Uddin",
+        date: "2022-01-16T17:57:28.556094Z",
+      },
+    ],
+  },
+];
