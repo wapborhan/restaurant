332327663a48b52a2ab01ece951fd7170349ff75
Borhan Uddin <borhaninfos@gmail.com>
2022-01-21 14:07:41 +0600

    update

--- src/components/body/Menu.js
+++ src/components/body/Menu.js
@@ -1,2 +1,18 @@
 import React, { Component } from "react";
 import DISHES from "../../data/dishes";
+
+class Menu extends Component {
+  state = {
+    dishes: DISHES,
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6"></div>
+        </div>
+      </div>
+    );
+  }
+}
