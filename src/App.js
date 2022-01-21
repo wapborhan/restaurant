7063d40c412c4e96ef4a67eda601eb5f1699e1ba
Borhan Uddin <borhaninfos@gmail.com>
2022-01-21 13:29:13 +0600

    File Structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import MainComponent from "./components/MainComponent";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MainComponent />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./header/Header";
+import Body from "./body/Body";
+import Footer from "./footer/Footer";
+
+const MainComponent = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+
+export default MainComponent;
