05025057aa87bc3e21a3f030f99ef103b9811cb7
Borhan Uddin <borhaninfos@gmail.com>
2022-02-12 23:04:48 +0600

    changes

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -3,15 +3,18 @@ import Menu from "./Menu";
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
-import { Routes, Route, Redirect } from "react-router-dom";
+import NotFound from "../NotFound";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const Body = () => {
   return (
     <Routes>
-      <Route path="/" exact element={<Home />} />
+      <Route path="/home" exact element={<Home />} />
       <Route path="/menu" exact element={<Menu />} />
       <Route path="/contact" exact element={<Contact />} />
       <Route path="/about" exact element={<About />} />
+      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<Navigate replace to="/home" />} />
     </Routes>
   );
 };
